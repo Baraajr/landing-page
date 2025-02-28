63f829c4ad489367237b3a5f753dda864944a902
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-02-28 10:05:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function changePhone(phone) {
+  img.src = phone;
+}
+
+function changeColor(color) {
+  container.style.backgroundColor = color;
+}
